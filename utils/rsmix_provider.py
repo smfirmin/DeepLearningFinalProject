6a279c75f54e7638699e93e02472f2d6ab1be091
Isaac Zaydens <isaac.zaydens@gmail.com>
2021-12-05 15:29:20 -0800

    added torch to rsmix
    Signed-off-by: Isaac Zaydens <isaac.zaydens@gmail.com>

--- utils/rsmix_provider.py
+++ utils/rsmix_provider.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 # import h5py
 # import tensorflow as tf
+import torch
 import random
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
@@ -83,12 +84,12 @@ def square_distance(src, dst):
     """
     B, N, _ = src.shape
     _, M, _ = dst.shape
-    # dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
-    # dist += torch.sum(src ** 2, -1).view(B, N, 1)
-    # dist += torch.sum(dst ** 2, -1).view(B, 1, M)
-    dist = -2 * np.matmul(src, dst.transpose(0, 2, 1))
-    dist += np.sum(src ** 2, -1).reshape(B, N, 1)
-    dist += np.sum(dst ** 2, -1).reshape(B, 1, M)
+    dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
+    dist += torch.sum(src ** 2, -1).view(B, N, 1)
+    dist += torch.sum(dst ** 2, -1).view(B, 1, M)
+    # dist = -2 * np.matmul(src, dst.transpose(0, 2, 1))
+    # dist += np.sum(src ** 2, -1).reshape(B, N, 1)
+    # dist += np.sum(dst ** 2, -1).reshape(B, 1, M)
     return dist
 
 
