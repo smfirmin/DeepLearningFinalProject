c0bb07f341bc9c2c22746db28b425b5823ce3db0
sfirmin <smfirmin@gmail.com>
2021-12-05 21:08:58 -0700

    add meta-pu repo

---
+++ interpolation/setup.py
@@ -0,0 +1,17 @@
+from __future__ import division, absolute_import, with_statement, print_function
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import glob
+import shutil
+
+import pointnet2
+
+requirements = ["hydra-core==0.11.3", "pytorch-lightning==0.7.1", "h5py", "pprint", "enum34", "future"]
+
+setup(
+    name="pointnet2",
+    version=__version__,
+    author="Erik Wijmans",
+    packages=find_packages(),
+    install_requires=requirements,
+)
