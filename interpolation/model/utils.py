65f229ad998f765cc2ba613e3c83aaad3a27becb
sfirmin <smfirmin@gmail.com>
2021-12-06 19:43:32 -0700

    change pointnet2 import

--- interpolation/model/utils.py
+++ interpolation/model/utils.py
@@ -17,8 +17,7 @@ except:
     import __builtin__ as builtins
 
 try:
-    # import pointnet2._ext as _ext
-    import _ext as _ext
+    import pointnet2._ext as _ext
 except ImportError:
     if not getattr(builtins, "__POINTNET2_SETUP__", False):
         raise ImportError(
