1ad6eee1d78f0c9c0015870816af57cecdd0b59d
sfirmin <smfirmin@gmail.com>
2021-12-06 20:04:53 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-# install using 'pip install -e .'
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import glob
+import shutil
 
-from setuptools import setup
+_ext_src_root = "pointnet2_ops_lib/_ext-src/"
+_ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
+    "{}/src/*.cu".format(_ext_src_root)
+)
+_ext_headers = glob.glob("{}/include/*".format(_ext_src_root))
+
+requirements = ["etw_pytorch_utils==1.1.1", "h5py", "enum34", "future"]
 
 setup(name='pointnet',
       packages=['pointnet'],
